ec497f17ab349f4581c605ba2295201ce3a7de7c
yawen xue <124210098+yawenx2004@users.noreply.github.com>
2025-12-04 18:31:55 -0800

    fixed image sizing issue

--- frontend/src/pages/Art.jsx
+++ frontend/src/pages/Art.jsx
@@ -6,50 +6,62 @@ function Art(props) {
         {
             "title": "placeholder",
             "desc": "placeholder",
-            "img": "/art/hanged-man.PNG"
+            "img": "/art/hanged-man.jpg"
         },
         {
             "title": "placeholder",
             "desc": "placeholder",
-            "img": "/art/armand.GIF"
+            "img": "/art/armand.webm"
         },
         {
             "title": "placeholder",
             "desc": "placeholder",
-            "img": "/art/giordano.png"
+            "img": "/art/giordano.jpg"
         },
         {
             "title": "placeholder",
             "desc": "placeholder",
-            "img": "/art/collage.PNG"
+            "img": "/art/collage.jpg"
         },
         {
             "title": "placeholder",
             "desc": "placeholder",
-            "img": "/art/anim-1.gif"
+            "img": "/art/anim-1.webm"
         },
         {
             "title": "placeholder",
             "desc": "placeholder",
-            "img": "/art/boat.PNG"
+            "img": "/art/boat.jpg"
         }
     ]
 
     return (
         <div className="page">
             <h2>&gt;&gt; art</h2>
+
             <div className="post-area art">
-                {dummyArtposts.map(item => (
-                    <div className="post art"
-                    >
-                        <img
-                            src={item.img}
-                        />
-                    </div>
-                ))}
+            {dummyArtposts.map((item, i) => {
+                const isImage = item.img.endsWith(".webp") || item.img.endsWith(".png") || item.img.endsWith(".jpg");
+
+                return (
+                <div className="post art" key={i}>
+                    {isImage ? (
+                    <img src={item.img} loading="lazy" />
+                    ) : (
+                    <video
+                        src={item.img}
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
+                    />
+                    )}
+                </div>
+                );
+            })}
             </div>
         </div>
-    )
+    );
 }
 
 export default Art;
